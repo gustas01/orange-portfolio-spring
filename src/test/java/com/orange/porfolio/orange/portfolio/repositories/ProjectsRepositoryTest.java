ac22acf534a6365818d73ca5bf75b1a9b13d7b3d
Gustavo <gustavohdlima@hotmail.com>
2024-05-23 14:33:39 -0300

    test: Escrevendo os mesmos métodos que já estavam no ProjectsRepository, porém agora usando o JUnit invés do assertJ
    Os testes usando o assertJ estão comentados

--- src/test/java/com/orange/porfolio/orange/portfolio/repositories/ProjectsRepositoryTest.java
+++ src/test/java/com/orange/porfolio/orange/portfolio/repositories/ProjectsRepositoryTest.java
@@ -1,12 +1,9 @@
 package com.orange.porfolio.orange.portfolio.repositories;
 
 import com.orange.porfolio.orange.portfolio.DTOs.CreateProjectDTO;
-import com.orange.porfolio.orange.portfolio.DTOs.CreateTagDTO;
 import com.orange.porfolio.orange.portfolio.DTOs.CreateUserDTO;
-import com.orange.porfolio.orange.portfolio.DTOs.ProjectDTO;
 import com.orange.porfolio.orange.portfolio.entities.Project;
 import com.orange.porfolio.orange.portfolio.entities.Role;
-import com.orange.porfolio.orange.portfolio.entities.Tag;
 import com.orange.porfolio.orange.portfolio.entities.User;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
@@ -17,15 +14,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.assertj.core.api.Assertions.*;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+//import static org.assertj.core.api.Assertions.*;
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -51,13 +49,22 @@ class ProjectsRepositoryTest {
     Page<Project> projectDTOS = this.projectsRepository
                     .findAllByAuthorId(persistedProject.getAuthor().getId(), pageable);
 
-    assertThat(projectDTOS).isNotNull();
-    assertThat(projectDTOS.getContent().getFirst().getTitle()).isEqualTo("um título");
-    assertThat(projectDTOS.getContent().getFirst().getDescription()).isEqualTo("uma descrição");
-    assertThat(projectDTOS.getContent().getFirst().getUrl()).isEqualTo("http://www.umaurl.com.br");
-    assertThat(projectDTOS.getContent().getFirst().getAuthor().getFirstName()).isEqualTo("gustavo");
-    assertThat(projectDTOS.getContent().getFirst().getAuthor().getLastName()).isEqualTo("lima");
-    assertThat(projectDTOS.getContent().getFirst().getAuthor().getEmail()).isEqualTo("gustavo@email.com");
+    assertNotNull(projectDTOS);
+    assertEquals(projectDTOS.getContent().getFirst().getTitle(), "um título");
+    assertEquals(projectDTOS.getContent().getFirst().getDescription(), "uma descrição");
+    assertEquals(projectDTOS.getContent().getFirst().getUrl(), "http://www.umaurl.com.br");
+    assertEquals(projectDTOS.getContent().getFirst().getAuthor().getFirstName(), "gustavo");
+    assertEquals(projectDTOS.getContent().getFirst().getAuthor().getLastName(), "lima");
+    assertEquals(projectDTOS.getContent().getFirst().getAuthor().getEmail(), "gustavo@email.com");
+
+    //usando assertJ
+//    assertThat(projectDTOS).isNotNull();
+//    assertThat(projectDTOS.getContent().getFirst().getTitle()).isEqualTo("um título");
+//    assertThat(projectDTOS.getContent().getFirst().getDescription()).isEqualTo("uma descrição");
+//    assertThat(projectDTOS.getContent().getFirst().getUrl()).isEqualTo("http://www.umaurl.com.br");
+//    assertThat(projectDTOS.getContent().getFirst().getAuthor().getFirstName()).isEqualTo("gustavo");
+//    assertThat(projectDTOS.getContent().getFirst().getAuthor().getLastName()).isEqualTo("lima");
+//    assertThat(projectDTOS.getContent().getFirst().getAuthor().getEmail()).isEqualTo("gustavo@email.com");
   }
 
   @Test
@@ -73,8 +80,12 @@ class ProjectsRepositoryTest {
     Page<Project> projectDTOS = this.projectsRepository
             .findAllByAuthorId(persistedUser.getId(), pageable);
 
-    assertThat(projectDTOS.getContent()).isEmpty();
-    assertThat(projectDTOS.getNumberOfElements()).isEqualTo(0);
+    assertEquals(projectDTOS.getContent(), List.of());
+    assertEquals(projectDTOS.getNumberOfElements(), 0);
+
+    //usando assertJ
+//    assertThat(projectDTOS.getContent()).isEmpty();
+//    assertThat(projectDTOS.getNumberOfElements()).isEqualTo(0);
   }
 
   private Project createProject(CreateProjectDTO data){
