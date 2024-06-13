381609bc09540bc00026fcafb7fc553ac9191c18
Gustavo <gustavohdlima@hotmail.com>
2024-06-13 15:40:39 -0300

    integration test: testando busca de usuário pelo ID - GET /users/:id

--- src/test/java/com/orange/porfolio/orange/portfolio/OrangePortfolioApplicationTests.java
+++ src/test/java/com/orange/porfolio/orange/portfolio/OrangePortfolioApplicationTests.java
@@ -20,7 +20,9 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
@@ -54,26 +56,27 @@ class OrangePortfolioApplicationTests {
   private PasswordEncoder passwordEncoder;
 
   TestUtilsMocks mocksObjects;
+
   @BeforeEach
-  void setup(){
+  void setup() {
     mocksObjects = new TestUtilsMocks();
   }
 
   @BeforeAll
-  void seed(){
+  void seed() {
     String passwordAdmin = passwordEncoder.encode("12345678Aa!");
     jdbcTemplate.execute("INSERT INTO roles (id, name) VALUES (2, 'admin')");
     jdbcTemplate.update("INSERT INTO users (id, email, first_name, last_name, password, google, avatar_url) VALUES ('68665ad3-e29a-4491-ae33-33a340813563', 'admin@admin.com', 'Admin', 'User', ?, false, null)", (passwordAdmin));
     jdbcTemplate.execute("INSERT INTO users_roles (users_id, roles_id) VALUES ('68665ad3-e29a-4491-ae33-33a340813563', 2);");
   }
 
-	@Test
+  @Test
   @DisplayName("Should create an user")
   @Order(1)
-	void register() throws IOException {
+  void register() throws IOException {
     ObjectMapper mapper = new ObjectMapper();
 
-    String url = mocksObjects.mockUrl+port+"/auth/register";
+    String url = mocksObjects.mockUrl + port + "/auth/register";
     CreateUserDTO mockCreateUserDTO = mocksObjects.mockCreateUserDTO;
     MockMultipartFile mockMultipartFile = mocksObjects.mockMultipartFileImage;
 
@@ -85,7 +88,7 @@ class OrangePortfolioApplicationTests {
     formHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
 
     MultiValueMap<String, Object> formData = new LinkedMultiValueMap<>();
-    formData.add("data", new HttpEntity<>(mapper.writeValueAsString(mockCreateUserDTO), dataHeaders) );
+    formData.add("data", new HttpEntity<>(mapper.writeValueAsString(mockCreateUserDTO), dataHeaders));
     formData.add("image", new HttpEntity<>(mockMultipartFile.getBytes(), fileHeaders));
 
     HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(formData, formHeaders);
@@ -99,7 +102,7 @@ class OrangePortfolioApplicationTests {
     assertEquals(mockCreateUserDTO.getEmail(), user.get().getEmail());
     assertEquals("201 CREATED", response.getStatusCode().toString());
 
-	}
+  }
 
 
   @Test
@@ -107,7 +110,7 @@ class OrangePortfolioApplicationTests {
   @Order(2)
   void login() {
     LoginUserDTO loginUserDTO = mocksObjects.mockLoginUserDTO;
-    String url = mocksObjects.mockUrl+port+"/auth/login";
+    String url = mocksObjects.mockUrl + port + "/auth/login";
 
     ResponseEntity<String> response = testRestTemplate.postForEntity(url, loginUserDTO, String.class);
 
@@ -128,7 +131,7 @@ class OrangePortfolioApplicationTests {
   @DisplayName("Should login an user as Admin")
   @Order(2)
   void loginAsAdmin() {
-    String url = mocksObjects.mockUrl+port+"/auth/login";
+    String url = mocksObjects.mockUrl + port + "/auth/login";
     LoginUserDTO loginAdminDTO = new LoginUserDTO("admin@admin.com", "12345678Aa!");
     ResponseEntity<String> response = testRestTemplate.postForEntity(url, loginAdminDTO, String.class);
 
@@ -148,8 +151,8 @@ class OrangePortfolioApplicationTests {
   @Test
   @DisplayName("Should make a request and throw an exception due to the user not being logged in")
   @Order(3)
-  void requestFail(){
-    String url = mocksObjects.mockUrl+port+"/me/data";
+  void requestFail() {
+    String url = mocksObjects.mockUrl + port + "/me/data";
 
     ResponseEntity<StandardError> response = testRestTemplate.getForEntity(url, StandardError.class);
 
@@ -162,12 +165,12 @@ class OrangePortfolioApplicationTests {
   @Test
   @DisplayName("Should TRY to create a Tag and throw an exception due to permission")
   @Order(3)
-  void createTagFailure(){
+  void createTagFailure() {
     Tag mockTag = mocksObjects.mockTag;
-    String url = mocksObjects.mockUrl+port+"/tags";
+    String url = mocksObjects.mockUrl + port + "/tags";
 
     HttpHeaders headersWithCookies = new HttpHeaders();
-    headersWithCookies.set(HttpHeaders.COOKIE, "token="+userToken);
+    headersWithCookies.set(HttpHeaders.COOKIE, "token=" + userToken);
     headersWithCookies.setContentType(MediaType.APPLICATION_JSON);
 
     HttpEntity<Tag> entityWithCookies = new HttpEntity<>(mockTag, headersWithCookies);
@@ -183,13 +186,13 @@ class OrangePortfolioApplicationTests {
   @Test
   @DisplayName("Should create a Tag logged as admin")
   @Order(3)
-  void createTagAsAdmin(){
+  void createTagAsAdmin() {
     CreateTagDTO mockCreateTagDTO = mocksObjects.mockCreateTagDTO;
     CreateTagDTO createTagDTO2 = mocksObjects.mockCreateTagDTO2;
-    String url = mocksObjects.mockUrl+port+"/tags";
+    String url = mocksObjects.mockUrl + port + "/tags";
 
     HttpHeaders headersWithCookies = new HttpHeaders();
-    headersWithCookies.set(HttpHeaders.COOKIE, "token="+adminToken);
+    headersWithCookies.set(HttpHeaders.COOKIE, "token=" + adminToken);
     headersWithCookies.setContentType(MediaType.APPLICATION_JSON);
 
     HttpEntity<CreateTagDTO> entityWithCookies = new HttpEntity<>(mockCreateTagDTO, headersWithCookies);
@@ -208,11 +211,11 @@ class OrangePortfolioApplicationTests {
   @Test
   @DisplayName("Should TRY to update a Tag and throw an exception due to permission")
   @Order(4)
-  void updateTagFailure(){
-    String url = mocksObjects.mockUrl+port+"/tags/"+2;
+  void updateTagFailure() {
+    String url = mocksObjects.mockUrl + port + "/tags/" + 2;
 
     HttpHeaders headersWithCookies = new HttpHeaders();
-    headersWithCookies.set(HttpHeaders.COOKIE, "token="+userToken);
+    headersWithCookies.set(HttpHeaders.COOKIE, "token=" + userToken);
     headersWithCookies.setContentType(MediaType.APPLICATION_JSON);
 
     CreateTagDTO updateDataTag = new CreateTagDTO("frontend editado");
@@ -230,11 +233,11 @@ class OrangePortfolioApplicationTests {
   @Test
   @DisplayName("Should update a Tag logged as admin")
   @Order(4)
-  void updateTagAsAdmin(){
-    String url = mocksObjects.mockUrl+port+"/tags/"+2;
+  void updateTagAsAdmin() {
+    String url = mocksObjects.mockUrl + port + "/tags/" + 2;
 
     HttpHeaders headersWithCookies = new HttpHeaders();
-    headersWithCookies.set(HttpHeaders.COOKIE, "token="+adminToken);
+    headersWithCookies.set(HttpHeaders.COOKIE, "token=" + adminToken);
     headersWithCookies.setContentType(MediaType.APPLICATION_JSON);
 
     CreateTagDTO updateDataTag = new CreateTagDTO("frontend editado");
@@ -255,11 +258,11 @@ class OrangePortfolioApplicationTests {
   @Test
   @DisplayName("Should TRY to delete a Tag and throw an exception due to permission")
   @Order(5)
-  void deleteTagFailure(){
-    String url = mocksObjects.mockUrl+port+"/tags/"+2;
+  void deleteTagFailure() {
+    String url = mocksObjects.mockUrl + port + "/tags/" + 2;
 
     HttpHeaders headersWithCookies = new HttpHeaders();
-    headersWithCookies.set(HttpHeaders.COOKIE, "token="+userToken);
+    headersWithCookies.set(HttpHeaders.COOKIE, "token=" + userToken);
     headersWithCookies.setContentType(MediaType.APPLICATION_JSON);
 
     HttpEntity<CreateTagDTO> entityWithCookies = new HttpEntity<>(headersWithCookies);
@@ -275,11 +278,11 @@ class OrangePortfolioApplicationTests {
   @Test
   @DisplayName("Should delete a Tag logged as admin")
   @Order(5)
-  void deleteTagAsAdmin(){
-    String url = mocksObjects.mockUrl+port+"/tags/"+2;
+  void deleteTagAsAdmin() {
+    String url = mocksObjects.mockUrl + port + "/tags/" + 2;
 
     HttpHeaders headersWithCookies = new HttpHeaders();
-    headersWithCookies.set(HttpHeaders.COOKIE, "token="+adminToken);
+    headersWithCookies.set(HttpHeaders.COOKIE, "token=" + adminToken);
     headersWithCookies.setContentType(MediaType.APPLICATION_JSON);
 
     HttpEntity<Object> entityWithCookies = new HttpEntity<>(headersWithCookies);
@@ -299,13 +302,13 @@ class OrangePortfolioApplicationTests {
   @Test
   @DisplayName("Should return the data of the logged user")
   @Order(3)
-  void userData(){
+  void userData() {
     CreateUserDTO mockCreateUserDTO = mocksObjects.mockCreateUserDTO;
 
-    String url = mocksObjects.mockUrl+port+"/users/me/data";
+    String url = mocksObjects.mockUrl + port + "/users/me/data";
 
     HttpHeaders headersWithCookies = new HttpHeaders();
-    headersWithCookies.set(HttpHeaders.COOKIE, "token="+userToken);
+    headersWithCookies.set(HttpHeaders.COOKIE, "token=" + userToken);
     headersWithCookies.setContentType(MediaType.APPLICATION_JSON);
 
     HttpEntity<Object> entityWithCookies = new HttpEntity<>(headersWithCookies);
@@ -327,18 +330,18 @@ class OrangePortfolioApplicationTests {
 
     ObjectMapper mapper = new ObjectMapper();
 
-    String url = mocksObjects.mockUrl+port+"/users";
+    String url = mocksObjects.mockUrl + port + "/users";
 
     HttpHeaders dataHeaders = new HttpHeaders();
     HttpHeaders formHeaders = new HttpHeaders();
     dataHeaders.setContentType(MediaType.APPLICATION_JSON);
     formHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
-    formHeaders.set(HttpHeaders.COOKIE, "token="+userToken);
+    formHeaders.set(HttpHeaders.COOKIE, "token=" + userToken);
 
     MultiValueMap<String, Object> formData = new LinkedMultiValueMap<>();
     HttpEntity<MultiValueMap<String, Object>> entityWithCookies = new HttpEntity<>(formData, formHeaders);
 
-    formData.add("data", new HttpEntity<>(mapper.writeValueAsString(mockUpdateUserDTO), dataHeaders) );
+    formData.add("data", new HttpEntity<>(mapper.writeValueAsString(mockUpdateUserDTO), dataHeaders));
 
     ResponseEntity<UserDTO> response = testRestTemplate.exchange(url, HttpMethod.PUT, entityWithCookies, UserDTO.class);
 
@@ -346,5 +349,31 @@ class OrangePortfolioApplicationTests {
     assertEquals(mockUpdateUserDTO.getEmail(), Objects.requireNonNull(response.getBody()).getEmail());
     assertEquals(mockUpdateUserDTO.getFirstName(), response.getBody().getFirstName());
     assertEquals(mockUpdateUserDTO.getLastName(), response.getBody().getLastName());
+  }
+
+
+  @Test
+  @DisplayName("Should return the data of the user by id")
+  @Order(3)
+  void findOneUser() {
+    CreateUserDTO createUserDTO = mocksObjects.mockCreateUserDTO;
+    Optional<User> user = userRepository.findByEmail(createUserDTO.getEmail());
+
+    assertTrue(user.isPresent());
+
+    String url = mocksObjects.mockUrl + port + "/users/"+user.get().getId();
+
+    HttpHeaders headersWithCookies = new HttpHeaders();
+    headersWithCookies.set(HttpHeaders.COOKIE, "token=" + userToken);
+    headersWithCookies.setContentType(MediaType.APPLICATION_JSON);
+
+    HttpEntity<Object> entityWithCookies = new HttpEntity<>(headersWithCookies);
+
+    ResponseEntity<UserDTO> response = testRestTemplate.exchange(url, HttpMethod.GET, entityWithCookies, UserDTO.class);
+
+    assertEquals("200 OK", response.getStatusCode().toString());
+    assertEquals(createUserDTO.getEmail(), Objects.requireNonNull(response.getBody()).getEmail());
+    assertEquals(createUserDTO.getFirstName(), response.getBody().getFirstName());
+    assertEquals(createUserDTO.getLastName(), response.getBody().getLastName());
   }
 }
